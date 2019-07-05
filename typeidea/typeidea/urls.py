82a5faa775742f668ad52d578e9f988e5c2d8f4d
段丹阳 <duan_danyang@163.com>
2019-07-05 23:14:48 +0800

    配置 django-debug-toolbar

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -56,3 +56,9 @@ urlpatterns = [
     url(r'^api/', include(router.urls, namespace="api")),
     url(r'api/docs/', include_docs_urls(title='typeidea apis')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns

4c81f36c8e81f63ff5b7598a626eef4bf775ecb2
段丹阳 <duandanyang@xiaozhu.com>
2019-07-09 19:13:11 +0800

    配置silk

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -10,11 +10,11 @@ DATABASES = {
 }
 
 INSTALLED_APPS += [
-    'debug_toolbar',
+    'silk',
 ]
 
 MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    import silk
+    urlpatterns += [
+        url(r'^silk/', include('silk.urls', namespace='silk')),
+    ]

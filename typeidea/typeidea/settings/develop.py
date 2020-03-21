749a2e7e4b5210fa79b45c93e9a57f9f4cf1087a
段丹阳 <duandanyang@xiaozhu.com>
2020-03-21 15:11:46 +0800

    update

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -7,8 +7,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'typeidea_db',
         'USER': 'root',
-        'PASSWORD': 'u09021232',
-        'HOST': '10.0.2.199',
+        'PASSWORD': 'Admin123',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         #'CONN_MAX_AGE': 5 * 60,
         #'OPTIONS': {'charset': 'utf8mb4'}
